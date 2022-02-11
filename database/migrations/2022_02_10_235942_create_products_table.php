7d1b4b7c75917bdd0b4e23ecd356d2771e95107e
2021 DSM53 Duarte V Fernando <al222010329@gmail.com>
2022-02-10 18:04:12 -0600

    Modificamos la estructura de la tabla para productos

--- database/migrations/2022_02_10_235942_create_products_table.php
+++ database/migrations/2022_02_10_235942_create_products_table.php
@@ -14,7 +14,12 @@ return new class extends Migration
     public function up()
     {
         Schema::create('products', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->bigInteger('precio');
+            $table->bigInteger('cantidad');
+            $table->unsignedBigInteger('category_id');
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('cascade');
             $table->timestamps();
         });
     }
