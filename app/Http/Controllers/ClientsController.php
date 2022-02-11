ffbea7bd9c1cc6451a863f87f4aea54796df070d
2021 DSM53 Duarte V Fernando <al222010329@gmail.com>
2022-02-10 18:00:52 -0600

    Creamos el modelo de clientes y ventas

---
+++ app/Http/Controllers/ClientsController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreClientsRequest;
+use App\Http\Requests\UpdateClientsRequest;
+use App\Models\Clients;
+
+class ClientsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreClientsRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreClientsRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Clients  $clients
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Clients $clients)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Clients  $clients
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Clients $clients)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateClientsRequest  $request
+     * @param  \App\Models\Clients  $clients
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateClientsRequest $request, Clients $clients)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Clients  $clients
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Clients $clients)
+    {
+        //
+    }
+}
