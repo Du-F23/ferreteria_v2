1c6b6c15ebc2b8d401dc06bd4f580e54457e628f
2021 DSM53 Duarte V Fernando <al222010329@gmail.com>
2022-02-10 18:02:15 -0600

    Modificamos la estructura de la tabla para categorias

--- database/migrations/2022_02_10_235747_create_categories_table.php
+++ database/migrations/2022_02_10_235747_create_categories_table.php
@@ -14,7 +14,8 @@ return new class extends Migration
     public function up()
     {
         Schema::create('categories', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements('id');
+            $table->string('name');
             $table->timestamps();
         });
     }
